59c8180ed438c933545e8e54e2a10d758ce4c441
tsdobbs <tim.dobbs@gmail.com>
2019-07-13 20:37:10 -0700

    Added color parsing

--- web/views.py
+++ web/views.py
@@ -1,10 +1,21 @@
 #views.py - Maps URLs to backend functions, then returns the results to the appropriate view
 
 import datetime
+import json
 from __init__ import app, db, models, csrf
 from flask import render_template, flash, redirect, url_for, request, Response
 from sqlalchemy import desc, extract, sql
 from flask_login import login_required
+
+def parse_rgb(rgb_str):
+    #converts an rgb string into a list and returns which color is biggest
+    str_list = rgb_str[rgb_str.find('(') + 1: rgb_str.find(')')
+                 ].split(',')
+    int_list = [int(color.replace(" ","")) for color in str_list]
+    max_position = int_list.index(max(int_list))
+    max_color = ['red','green','blue'][max_position]
+    return max_color
+
 
 #Home page
 @app.route('/', methods=('GET', 'POST'))
@@ -14,14 +25,21 @@ def index():
 
 @app.route('/receive_art', methods=['POST'])
 def receive_art():
-    print(request.headers)
     art = request.json
+
+
+    standardized_art = {'red':[]
+                       ,'green':[]
+                       ,'blue':[]}
+    for color in art:
+        max_col = parse_rgb(color)
+        standardized_art[max_col] += art[color]
 
     art_data = dict()
     art_data['title'] = 'My Cool Test Art'
     art_data['email'] = 'raddude88@chillmail.com'
     art_data['submit_date'] = datetime.datetime.now()
-    art_data['art'] = str(art)
+    art_data['art'] = json.dumps(standardized_art)
     art_data['status'] = 'Submitted'
 
     db.session.add(models.artpieces(**art_data))
