560001ff3299f733c8cb8bda8a4857b34651e928
tsdobbs <tim.dobbs@gmail.com>
2019-07-17 21:56:06 -0700

    Wrote version that uses Python template instead of JSON. Pipettes but physical technical issues

--- robot/art_processor.py
+++ robot/art_processor.py
@@ -1,38 +1,52 @@
 import json
-import datetime.datetime
+import sqlalchemy as db
+import pandas as pd #pandas is overkill, but it makes the database work really really easy, and that's nice
+import string
+from datetime import datetime
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.abspath(os.path.join(basedir, os.pardir, 'ARTBot.db'))
+SQL_ENGINE = db.create_engine(SQLALCHEMY_DATABASE_URI)
+artpieces = pd.read_sql("SELECT * FROM artpieces WHERE status = 'Submitted'",SQL_ENGINE, parse_dates = ['submit_date'])
+artpieces['art'] = artpieces.art.apply(json.loads)
 
 #Lists slots that should typically be available
 def canvas_slot_generator():
-    for slot in [1,2,4,5,7,8,9,10,11]:
+    for slot in [1,2,3,4,5,6,7,8,9]:
         yield str(slot)
-get_canvas_slot = slotter()
+get_canvas_slot = canvas_slot_generator()
+
+def well_map(well):
+    map = dict(zip(range(26), string.ascii_uppercase))
+    letter = map[well[0]]
+    number = well[1] + 1
+    return letter + str(number)
 
 
 #Get JSON template
-template_file = open('ART_TEMPLATE.json')
-output_json = json.load(template_file)
+template_file = open('ART_TEMPLATE.txt')
+template_string = template_file.read()
 template_file.close()
 
-#Get list of dicts describing art
-input_file = open('ART_INPUT.json')
-input_json = json.load(input_file)
-input_file.close()
+def make_procedure(artpiece):
+    art = artpiece.art
+    for color in art:
+        art[color] = [well_map(well) for well in art[color]]
+    #This works for one canvas. Jinja templating might actually be a better way to do this for multiple canvases
+    canvas_string = template_string.replace('%%RED WELLS GO HERE%%',str(art['red'])[1:-1])
+    canvas_string = canvas_string.replace('%%BLUE WELLS GO HERE%%', str(art['blue'])[1:-1])
+    canvas_string = canvas_string.replace('%%GREEN WELLS GO HERE%%', str(art['green'])[1:-1])
+
+    return canvas_string
 
 
+canvas_procedures = artpieces.iloc[:1].apply(make_procedure, axis=1)
+final_procedure_string = '\n\n'.join(canvas_procedures.tolist())
 
-#Make adjustments
-output_json['metadata']['created'] = str(datetime.now())
+unique_file_name = 'ARTISTIC_PROCEDURE_%s.py' % datetime.now().strftime("%Y%m%d-%H%M%S")
+output_file = open(os.path.join(basedir,'procedures',unique_file_name),'w')
+output_file.write(final_procedure_string)
+output_file.close()
 
-for num, artpiece in enumerate(input_json):
-	output_json['labware']['canvas%s' % num] = {
-		'slot': next(get_canvas_slot)
-		,'display-name': 'Canvas %s' % num
-		,'model': 'CCL_canvas'
-	}
-
-#For each job, make one step in "procedures" for each color used
-# and one sub-step for each well to dispense to.
-# (Might be some tricky math to manage distributing large amounts)
-# Then each job just gets concatenated onto the end, so it's one big list
-
-#Can this JSON-schema handle notifications (or tweets????)
