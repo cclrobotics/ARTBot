e7af7a4114ce3fcecd3f5c1d6dbfd83832e09ca5
tsdobbs <tim.dobbs@gmail.com>
2019-10-05 18:26:22 -0700

    Changed server settings to serve globally

--- web/run.py
+++ web/run.py
@@ -1,4 +1,4 @@
 #!flask/bin/python
 # run.py - Calls the __init__.py function (which runs all other required code), then starts the Flask server
 from __init__ import app
-app.run(host='0.0.0.0',port=8080,debug=False)
+app.run(host='0.0.0.0',debug=False)
