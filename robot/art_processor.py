1c12985115138b611a7c736ebdf45cf14e5fb978
tsdobbs <tim.dobbs@gmail.com>
2019-10-09 15:54:46 -0700

    Procedure generation workflow messaging

--- robot/art_processor.py
+++ robot/art_processor.py
@@ -18,8 +18,44 @@ NOTEBOOK = parser.parse_args().notebook
 try:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 except:
+    print('No Database URL found. Falling back to SQLite database')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.abspath(os.path.join(basedir, os.pardir, 'ARTBot.db'))
 SQL_ENGINE = db.create_engine(SQLALCHEMY_DATABASE_URI)
+
+
+# Lists slots that should typically be available
+def canvas_slot_generator():
+    for slot in [1, 2, 3, 4, 5, 6, 7, 8, 9]:
+        yield str(slot)
+get_canvas_slot = canvas_slot_generator()
+
+def well_map(well):
+    map = dict(zip(range(26), string.ascii_uppercase))
+    letter = map[well[0]]
+    number = well[1] + 1
+    return letter + str(number)
+
+
+def add_canvas_locations(template_string, artpieces):
+    # write where canvas plates are to be placed into code
+    canvas_locations = dict(zip(artpieces.title, get_canvas_slot))
+    procedure = template_string.replace('%%CANVAS LOCATIONS GO HERE%%', str(canvas_locations))
+
+    return procedure, canvas_locations
+
+
+def add_pixel_locations(template_string, artpieces):
+    # write where to draw pixels on each plate into code. Listed by color to reduce contamination
+    pixels_by_color = dict()
+    for index, artpiece in artpieces.iterrows():
+        for color in artpiece.art:
+            if color not in pixels_by_color:
+                pixels_by_color[color] = dict()
+            pixels_by_color[color][artpiece.title] = [well_map(pixel) for pixel in artpiece.art[color]]
+    procedure = template_string.replace('%%PIXELS GO HERE%%', str(pixels_by_color))
+
+    return procedure
+
 
 num_pieces = 0
 while num_pieces not in range(1,10):
@@ -36,56 +72,33 @@ query = f"""SELECT * FROM artpieces
 artpieces = pd.read_sql(query, SQL_ENGINE, parse_dates = ['submit_date'])
 artpieces['art'] = artpieces.art.apply(json.loads)
 
-print(f'Loaded {len(artpieces)} pieces of art')
-print(artpieces[['title','email','submit_date']])
 
-#Lists slots that should typically be available
-def canvas_slot_generator():
-    for slot in [1,2,3,4,5,6,7,8,9]:
-        yield str(slot)
-get_canvas_slot = canvas_slot_generator()
+if not len(artpieces):
+    print('No new art found. All done.')
 
-def well_map(well):
-    map = dict(zip(range(26), string.ascii_uppercase))
-    letter = map[well[0]]
-    number = well[1] + 1
-    return letter + str(number)
-
-def add_canvas_locations(template_string, artpieces):
-    #write where canvas plates are to be placed into code
-    canvas_locations = dict(zip(artpieces.title,get_canvas_slot))
-    procedure = template_string.replace('%%CANVAS LOCATIONS GO HERE%%', str(canvas_locations))
-
-    return procedure
-
-def add_pixel_locations(template_string, artpieces):
-    #write where to draw pixels on each plate into code. Listed by color to reduce contamination
-    pixels_by_color = dict()
-    for index, artpiece in artpieces.iterrows():
-        for color in artpiece.art:
-            if color not in pixels_by_color:
-                pixels_by_color[color] = dict()
-            pixels_by_color[color][artpiece.title] = [well_map(pixel) for pixel in artpiece.art[color]]
-    procedure = template_string.replace('%%PIXELS GO HERE%%', str(pixels_by_color))
-
-    return procedure
+else:
+    print(f'Loaded {len(artpieces)} pieces of art')
+    print(artpieces[['id','title','email','submit_date']])
 
 
-#Get Python art procedure template
-file_extension = 'ipynb' if NOTEBOOK == True else 'py' #Use Jupyter notbook template or .py template
-template_file = open(f'ART_TEMPLATE.{file_extension}')
-template_string = template_file.read()
-template_file.close()
+    #Get Python art procedure template
+    file_extension = 'ipynb' if NOTEBOOK == True else 'py' #Use Jupyter notbook template or .py template
+    template_file = open(f'ART_TEMPLATE.{file_extension}')
+    template_string = template_file.read()
+    template_file.close()
 
 
-procedure = add_canvas_locations(template_string, artpieces)
+    procedure, canvas_locations = add_canvas_locations(template_string, artpieces)
 
-procedure = add_pixel_locations(procedure, artpieces)
+    procedure = add_pixel_locations(procedure, artpieces)
 
 
-now = datetime.now().strftime("%Y%m%d-%H%M%S")
-unique_file_name = f'ARTISTIC_PROCEDURE_{now}.{file_extension}'
-output_file = open(os.path.join(basedir,'procedures',unique_file_name),'w')
-output_file.write(procedure)
-output_file.close()
+    now = datetime.now().strftime("%Y%m%d-%H%M%S")
+    unique_file_name = f'ARTISTIC_PROCEDURE_{now}.{file_extension}'
+    output_file = open(os.path.join(basedir,'procedures',unique_file_name),'w')
+    output_file.write(procedure)
+    output_file.close()
 
+    print(f'Successfully generated artistic procedure into: ARTBot/robot/procedures/{unique_file_name}')
+    print('The following slots will be used:')
+    print('\n'.join([f'Slot {str(canvas_locations[key])}: "{key}"' for key in canvas_locations]))
