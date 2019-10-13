6fd167519cc9a0d5719cc76228c84cad76d7acb4
j3dev <j3pydev@gmail.com>
2019-10-13 12:26:54 -0700

    sketching out asm emailer script with functions and schedule methods

--- asm_emailer/asm_emailer.py
+++ asm_emailer/asm_emailer.py
@@ -3,17 +3,31 @@ from web import app
 from Flask import Flask
 from flask_mail import Message, Mail
 
+# initiate mail with app config
 mail = Mail()
 app = Flask(__name__)
 mail.init_app(app)
 
-msg = Message("Hello",
-            sender="from@example.com",
-            recipients=["to@example.com"])
+def sendEmail():
+    msg = Message("Hello",
+                sender="from@example.com",
+                recipients=["to@example.com"])
 
-msg.recipients = ["you@example.com"]
-msg.add_recipient("somebodyelse@example.com")
+    msg.recipients = ["you@example.com"]
+    msg.add_recipient("somebodyelse@example.com")
 
-msg.html = "<b>testing</b>"
+    msg.html = "<b>testing</b>"
 
-mail.send(msg)
+    mail.send(msg)
+
+def getCompletedArt():
+
+def getArtSendEmail():
+
+schedule.every().day.at("7am").do(getArtSendEmail)
+schedule.every().day.at("2pm").do(getArtSendEmail)
+schedule.every().day.at("11pm").do(getArtSendEmail)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
