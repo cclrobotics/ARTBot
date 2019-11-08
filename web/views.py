b8231048cbc82f7aa39eab65f44c7c00f4f1f6e0
tsdobbs <tim.dobbs@gmail.com>
2019-11-07 22:29:43 -0800

    Added Paypal button to Over-Submission-Limit error message. Still need callback.

--- web/views.py
+++ web/views.py
@@ -21,6 +21,10 @@ def index():
         limit_message = None
     return render_template('main.html', limit_message=limit_message)
 
+@app.route('/donated',methods=['POST'])
+def donated():
+    return render_template('main.html', limit_message=request)
+
 @app.route('/receive_art', methods=['POST'])
 def receive_art():
     SUBMISSION_COUNT = models.site_vars.query.filter_by(var='SUBMISSION_CNT').first()
@@ -69,6 +73,47 @@ def receive_art():
 
     return 'Robot Art Loaded'
 
+@app.route('/paypal_confirm', methods=['POST'])
+def paypal_confirm():
+    # !/usr/bin/python
+
+    '''This module processes PayPal Instant Payment Notification messages (IPNs).'''
+
+    import sys
+    import urllib.parse
+    import requests
+
+    VERIFY_URL_PROD = 'https://ipnpb.paypal.com/cgi-bin/webscr'
+    VERIFY_URL_TEST = 'https://ipnpb.sandbox.paypal.com/cgi-bin/webscr'
+
+    # Switch as appropriate
+    VERIFY_URL = VERIFY_URL_TEST
+
+    # CGI preamble
+    print('content-type: text/plain')
+    print()
+
+    # Read and parse query string
+    param_str = sys.stdin.readline().strip()
+    params = urllib.parse.parse_qsl(param_str)
+
+    # Add '_notify-validate' parameter
+    params.append(('cmd', '_notify-validate'))
+
+    # Post back to PayPal for validation
+
+    headers = {'content-type': 'application/x-www-form-urlencoded',
+               'user-agent': 'Python-IPN-Verification-Script'}
+    r = requests.post(VERIFY_URL, params=params, headers=headers, verify=True)
+    r.raise_for_status()
+
+    # Check return message and take action as needed
+    if r.text == 'VERIFIED':
+        print('VERIFIED')
+    elif r.text == 'INVALID':
+        print('NOT VERIFIED')
+    else:
+        print('FAIL')
 
 @app.errorhandler(404)
 def page_not_found(e):
