c77019e371649a4908c23463a5844a68939203e2
Benjamin Bell <benjamin.bell-2@postgrad.manchester.ac.uk>
2019-11-26 14:54:31 -0800

    [fix] Artpiece id should not be nullable

--- web/database/models.py
+++ web/database/models.py
@@ -43,7 +43,7 @@ class EmailFailureState(Enum):
 class EmailFailureModel(SurrogatePK, Model):
     __tablename__ = 'emailfailures'
 
-    artpiece_id = Column(db.Integer, db.ForeignKey('artpieces.id'))
+    artpiece_id = Column(db.Integer, db.ForeignKey('artpieces.id'), nullable=False)
     state = Column(db.Enum(EmailFailureState, values_callable=lambda x: [e.value for e in x])
             , nullable=False, name='failure_state')
     error_msg = Column(db.String(150), nullable=False)
