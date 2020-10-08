bea96f7c7be8cc464d8992906807c5038432b78d
Tim Dobbs <tim.dobbs@gmail.com>
2020-10-07 17:01:35 -0700

    added pilot art to gallery, random display order

--- web/api/user/utilities.py
+++ web/api/user/utilities.py
@@ -1,6 +1,7 @@
 import os
 from PIL import Image
 from .artpiece import Artpiece
+import random
 
 #function to pull image off of database
 def pull_picture(id):
@@ -13,6 +14,7 @@ def pull_picture(id):
 """
 Return a list of images in the 'gallery' folder and their descriptions
 Output is list of tuples (image_location, image_description)
+    output list is in random order for random display order every time
 """
 def get_gallery_images():
     internal_gallery_path = './web/static/img/gallery/'
@@ -24,7 +26,10 @@ def get_gallery_images():
     for img_name in imagename_list:
         with Image.open(img_name) as image:
             desc_list.append(image.getexif().get(270)) #Exif ID 270 = ImageDescription
+            #now check aspect ratio and normalize
     imagename_list = [public_gallery_path + filename for filename in os.listdir(internal_gallery_path)]
     
     img_list = list(zip(imagename_list,desc_list))
+    random.shuffle(img_list)
+
     return img_list
