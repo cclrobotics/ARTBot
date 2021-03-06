5d527085f29d0b11a1b3834e0041fc0ad1d2bcf2
Tim Dobbs <tim.dobbs@gmail.com>
2021-03-05 23:11:13 -0800

    update names to apiV2, add safety tip pickup

--- web/robot/ART_TEMPLATE.py
+++ web/robot/ART_TEMPLATE.py
@@ -1,8 +1,9 @@
 from opentrons import protocol_api
+from opentrons.types import Location
 import math
 
 metadata = {
-    'apiLevel': '2.0',
+    'apiLevel': '2.8',
     'protocolName': 'CCL ARTBot',
     'author': 'Tim Dobbs and Counter Culture Labs',
     'source': 'ARTBot Protocol Builder',
@@ -12,49 +13,49 @@ metadata = {
     }
 
 
-def distribute_to_agar(self, vol, source, destination, disposal_vol):
-    self.max_volume = 20
+def distribute_to_agar(pipette, vol, source, destination, disposal_vol):
+    max_volume = pipette.max_volume
 
     dest = list(destination)  # allows for non-lists
 
     for cnt, well in enumerate(dest):
         if (cnt + 1) % 150 == 0:
-            self.drop_tip()
-            self.current_volume = 0
+            pipette.drop_tip()
 
-        if not self.current_tip():
-            self.pick_up_tip()
+        if not pipette.has_tip:
+            pipette.pick_up_tip()
 
-        if self.current_volume < (vol + disposal_vol):
+        if pipette.current_volume < (vol + disposal_vol):
             remaining_wells = len(dest) - cnt
             remaining_vol = remaining_wells * vol
 
-            if remaining_vol + disposal_vol > self.max_volume:
-                asp_vol = math.floor((self.max_volume - disposal_vol) / vol) * vol + disposal_vol - self.current_volume
+            if remaining_vol + disposal_vol > max_volume:
+                asp_vol = math.floor((max_volume - disposal_vol) / vol) * vol + disposal_vol - pipette.current_volume
             else:
-                asp_vol = remaining_vol + disposal_vol - self.current_volume
+                asp_vol = remaining_vol + disposal_vol - pipette.current_volume
 
-            self.aspirate(asp_vol, source)
+            pipette.aspirate(asp_vol, source)
 
-        self.move_to(well, strategy='arc')
-        self.dispense(vol)
-
-
-    self.drop_tip()
+        pipette.move_to(well)
+        pipette.dispense(vol)
 
 
-def run(protocol: protocol_api.ProtocolContext):
-    # set the pipette we will be using
-    pipette = protocol.load_instrument.%%PIPETTE GOES HERE%%(
-            mount='left',
-            tip_racks=[tiprack]
-    )
-    
+    pipette.drop_tip()
+
+
+def run(protocol: protocol_api.ProtocolContext):  
     # a tip rack for our pipette
     tiprack = protocol.load_labware('%%TIPRACK GOES HERE%%', 10)
 
     # a plate for all of the colors in our pallette
     palette = protocol.load_labware('%%PALETTE GOES HERE%%', 11)
+
+    # set the pipette we will be using
+    pipette = protocol.load_instrument(
+            '%%PIPETTE GOES HERE%%',
+            mount='left',
+            tip_racks=[tiprack]
+    )
 
     # load all of the 
     pixels_by_color_by_artpiece = %%PIXELS GO HERE%%
@@ -69,14 +70,14 @@ def run(protocol: protocol_api.ProtocolContext):
 
     # colored culture locations
     palette_colors = { color: next(get_well) for color in pixels_by_color_by_artpiece.keys() }
+    protocol.comment('**CHECK BEFORE RUNNING** - Colors should be loaded into these wells:')
     for color in palette_colors:
-        print(f'{color_map[color]} -> {palette_colors[color]}')
-    input('Once you have filled the color plate, press enter to continue...')
+        protocol.comment(f'{color_map[color]} -> {palette_colors[color]}')
 
     # plates to create art in
     canvas_labware = dict()
     for art_title in canvas_locations:
-        canvas_labware[art_title] = protocol.load_labware('CCL_ARTBot_canvas', canvas_locations[art_title])
+        canvas_labware[art_title] = protocol.load_labware('ccl_artbot_canvas', canvas_locations[art_title])
 
     # wells to dispense each color material to
     pixels_by_color = dict()
@@ -85,15 +86,24 @@ def run(protocol: protocol_api.ProtocolContext):
         pixels_by_artpiece = pixels_by_color_by_artpiece[color]
         for art_title in pixels_by_artpiece:
             pixels_by_color[color] += [
-                (canvas_labware[art_title]
-                ,canvas_labware[art_title].wells('A1').from_center(x=pixel[0], y=pixel[1], z=-2.8).coordinates)
+                Location(
+                    point=canvas_labware[art_title].wells()[0].from_center_cartesian(x=pixel[0], y=pixel[1], z=-0.5),
+                    labware=canvas_labware[art_title]
+                 )
                 for pixel in pixels_by_artpiece[art_title]
             ]
             if not len(pixels_by_artpiece[art_title]): #single-well case
                 pixel = pixels_by_artpiece[art_title]
                 pixels_by_color[color] += [
-                    (canvas_labware[art_title]
-                    ,canvas_labware[art_title].wells('A1').from_center(x=pixel[0], y=pixel[1], z=-2.8).coordinates)]
+                    Location(
+                        point=canvas_labware[art_title].wells()[0].from_center_cartesian(x=pixel[0], y=pixel[1], z=-0.5),
+                        labware=canvas_labware[art_title]
+                    )
+                ]
+
+    #This is a quick check that the pipette is calibrated to pick up tip at the bottom row as well as the top
+    pipette.pick_up_tip(tiprack.wells()[7])
+    pipette.return_tip()
 
     for color in pixels_by_color:
-        distribute_to_agar(pipette, 0.1, palette_colors[color], pixels_by_color[color], disposal_vol=4)
+        distribute_to_agar(pipette, 0.1, palette_colors[color], pixels_by_color[color], disposal_vol=1)
--- web/robot/processor_args.py
+++ web/robot/processor_args.py
@@ -7,11 +7,11 @@ parser.add_argument('--notebook'
                     ,help='Set this flag to output to a Jupyter Notebook instead of a .py file'
                     )
 parser.add_argument('--palette', '-pa'
-                    ,default='nunc_8_wellplate_flat'
+                    ,default='corning_96_wellplate_360ul_flat'
                     ,help='Optional argument to specify the kind of labware to use as the palette plate. Use Opentrons standard names.'
                     )
 parser.add_argument('--pipette', '-pi'
-                    ,default='P10_Single'
+                    ,default='p10_single'
                     ,help='Optional argument to specify the pipette type. Use Opentrons standard names.'
                     )
 
